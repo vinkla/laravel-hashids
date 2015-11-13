0f0e35740c160ae93821470e037f6abe2bc7f295
Vincent Klaiber <vincentklaiber@gmail.com>
2015-11-13 09:06:52 +0100

    Improved environment specific code

--- src/HashidsServiceProvider.php
+++ src/HashidsServiceProvider.php
@@ -11,8 +11,11 @@
 
 namespace Vinkla\Hashids;
 
+use Hashids\Hashids;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 
 /**
  * This is the Hashids service provider class.
@@ -42,9 +45,9 @@ class HashidsServiceProvider extends ServiceProvider
     {
         $source = realpath(__DIR__.'/../config/hashids.php');
 
-        if (class_exists('Illuminate\Foundation\Application', false) && $app->runningInConsole()) {
+        if ($app instanceof LaravelApplication && $app->runningInConsole()) {
             $this->publishes([$source => config_path('hashids.php')]);
-        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+        } elseif ($app instanceof LumenApplication) {
             $app->configure('hashids');
         }
 
@@ -113,7 +116,7 @@ class HashidsServiceProvider extends ServiceProvider
             return $manager->connection();
         });
 
-        $app->alias('hashids.connection', Client::class);
+        $app->alias('hashids.connection', Hashids::class);
     }
 
     /**
