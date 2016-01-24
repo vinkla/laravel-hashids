44f61793a350d3fd72b6f64b39a0b2287e6c3589
Vincent Klaiber <vincentklaiber@gmail.com>
2016-01-24 14:49:14 +0100

    Tweak config booting

--- src/HashidsServiceProvider.php
+++ src/HashidsServiceProvider.php
@@ -31,24 +31,22 @@ class HashidsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->setupConfig($this->app);
+        $this->setupConfig();
     }
 
     /**
      * Setup the config.
      *
-     * @param \Illuminate\Contracts\Container\Container $app
-     *
      * @return void
      */
-    protected function setupConfig(Application $app)
+    protected function setupConfig()
     {
         $source = realpath(__DIR__.'/../config/hashids.php');
 
-        if ($app instanceof LaravelApplication && $app->runningInConsole()) {
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
             $this->publishes([$source => config_path('hashids.php')]);
-        } elseif ($app instanceof LumenApplication) {
-            $app->configure('hashids');
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('hashids');
         }
 
         $this->mergeConfigFrom($source, 'hashids');
