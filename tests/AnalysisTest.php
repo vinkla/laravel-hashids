3b9ea8b7275e32b102084e8e1cccc860aa64fb74
Vincent Klaiber <vincentklaiber@gmail.com>
2017-08-07 10:47:13 +0200

    Add missing test

---
+++ tests/AnalysisTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of Laravel Hashids.
+ *
+ * (c) Vincent Klaiber <hello@vinkla.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Vinkla\Tests\Hashids;
+
+use GrahamCampbell\Analyzer\AnalysisTrait;
+use Laravel\Lumen\Application;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * This is the analysis test class.
+ *
+ * @author Vincent Klaiber <hello@vinkla.com>
+ */
+class AnalysisTest extends TestCase
+{
+    use AnalysisTrait;
+
+    /**
+     * Get the code paths to analyze.
+     *
+     * @return string[]
+     */
+    protected function getPaths()
+    {
+        return [
+            realpath(__DIR__.'/../config'),
+            realpath(__DIR__.'/../src'),
+            realpath(__DIR__),
+        ];
+    }
+
+    /**
+     * Get the classes to ignore not existing.
+     *
+     * @return string[]
+     */
+    protected function getIgnored()
+    {
+        return [Application::class];
+    }
+}
