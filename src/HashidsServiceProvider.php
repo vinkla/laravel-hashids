3500313348aa7d2176f7e6f5f554eeda68a3fe8b
Vincent Klaiber <vincentklaiber@gmail.com>
2018-01-02 19:59:57 +0100

    Fixed config in phars

--- src/HashidsServiceProvider.php
+++ src/HashidsServiceProvider.php
@@ -43,7 +43,7 @@ class HashidsServiceProvider extends ServiceProvider
      */
     protected function setupConfig()
     {
-        $source = realpath(__DIR__.'/../config/hashids.php');
+        $source = realpath($raw = __DIR__.'/../config/hashids.php') ?: $raw;
 
         if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
             $this->publishes([$source => config_path('hashids.php')]);
