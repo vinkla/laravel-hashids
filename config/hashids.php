240f4625994894f69357bea11647db4f78b01a92
Vincent Klaiber <vincent@doubledip.se>
2022-11-24 14:28:36 +0100

    Remove license from config

--- config/hashids.php
+++ config/hashids.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * Copyright (c) Vincent Klaiber.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @see https://github.com/vinkla/laravel-hashids
- */
-
 return [
 
     /*
